11c74033dd1fbb2f4e00ca70a077fe09a38a55c6
Rita-Gujar <ritagujar14@gmail.com>
2021-06-06 20:48:48 +0530

    Initial commit

---
+++ src/service/cricket-api.js
@@ -0,0 +1,17 @@
+const KEY = "QUEPdqe4e7fZO8gSYFUfnWBKuEz1";
+
+export const getMatches = () => {
+  const url = `https://cricapi.com/api/matches?apikey=${KEY}`;
+
+  return fetch(url)
+    .then((res) => res.json())
+    .catch((error) => console.log(error));
+};
+
+export const getMatchDetail = (id) => {
+  const url = `https://cricapi.com/api/cricketScore?apikey=${KEY}&unique_id=${id}`;
+
+  return fetch(url)
+    .then((res) => res.json())
+    .catch((error) => console.log(error));
+};
